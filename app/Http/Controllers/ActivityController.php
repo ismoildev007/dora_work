998093ac5b995fa420133dc87d4e0e0486057835
Quvonchbek_Jalilov <=>
2024-07-24 13:09:25 +0500

    some changed

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Activity;
 use App\Models\ActivityImage;
+use App\Models\Staff;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -19,7 +21,8 @@ class ActivityController extends Controller
     // Show the form for creating a new activity
     public function create()
     {
-        return view('admin.activities.create');
+        $staffs = Staff::all();
+        return view('admin.activities.create', compact('staffs'));
     }
 
     // Store a newly created activity in storage
